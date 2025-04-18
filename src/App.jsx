e624641d9b1fad31d0fb1022d9be21c2bc78ce50
Ashcroft-lab <iadarshgupta@gmail.com>
2025-04-18 13:56:52 +0530

    use effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 
 const Card = ({ title })=> {
+
+  const [count, setCount] = useState(0);
+
   const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log('Component mounted');
+
+  }, [hasLiked]);
+
+  useEffect(() => {
+    console.log('Card rendered');
+  }
+  , []);
+
+
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount((prevstate) => prevstate + 1)}>
+      <h2>{title} - {count}</h2>
       <button
         onClick={() => setHasLiked(!hasLiked)}>
           {hasLiked ? "Liked" : 'Like'}
